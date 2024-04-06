a50ae1dcfe8617805d48b85cf6235f98fd0637e3
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-04-06 12:43:34 -0400

    trying to fix betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+
 /**
  * bubble_sort - function that sorts an array of integers in ascendig order
  * @array: array of numbers
@@ -6,22 +7,21 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	unsigned int i, j;
+unsigned int i, j;
 
-	for (i = 0; i < size - 1; i++)
-	{
-		for (j = 0; j < (size - i - 1); j++)
-		{
-			if (array[j] > array[j + 1])
-			{
-				int temp;
+for (i = 0; i < size - 1; i++)
+{
+for (j = 0; j < (size - i - 1); j++)
+{
+if (array[j] > array[j + 1])
+{
+int temp;
 
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
-
-				print_array(array, size);
-			}
-		}
-	}
+temp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = temp;
+print_array(array, size);
+}
+}
+}
 }
